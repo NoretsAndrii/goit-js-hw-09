c2e191895419228574265f6c44ad540a63336706
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-02-12 23:46:19 +0200

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const emailForm = document.querySelector('input');
+const messageForm = document.querySelector('textarea');
+
+const saved = localStorage.getItem('feedback-form-state');
+const savedFormState = JSON.parse(saved);
+if (savedFormState !== null) {
+  emailForm.value = savedFormState.email;
+  messageForm.value = savedFormState.message;
+}
+
+feedbackForm.addEventListener('input', event => {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  const feedbackFormState = {
+    email,
+    message,
+  };
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+});
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+  if (email === '' || message === '') {
+    return alert('Всі поля форми повинні бути заповнени');
+  }
+  console.log({ email, message });
+  localStorage.removeItem('feedback-form-state');
+  feedbackForm.reset();
+});
